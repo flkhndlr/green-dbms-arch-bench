57dde96b2c10cc26a10623e0b58371c7b9b8b0da
flkhndlr <falk.haendler@googlemail.com>
2025-10-08 15:47:09 +0200

    add new scenario

--- less_resources/pg_tprocc_deleteschema.tcl
+++ less_resources/pg_tprocc_deleteschema.tcl
@@ -1,7 +1,6 @@
 #!/bin/tclsh
 # maintainer: Pooja Jain
 
-puts "SETTING CONFIGURATION"
 dbset db pg
 dbset bm TPC-C
 
@@ -9,10 +8,6 @@ diset connection pg_host postgres_container
 diset connection pg_port 5432
 diset connection pg_sslmode disable
 
-set vu [ numberOfCPUs ]
-set warehouse [ expr {$vu * 20} ]
-diset tpcc pg_count_ware $warehouse
-diset tpcc pg_num_vu $vu
 diset tpcc pg_superuser postgres
 diset tpcc pg_superuserpass postgres
 diset tpcc pg_defaultdbase postgres
@@ -20,8 +15,10 @@ diset tpcc pg_user postgres
 diset tpcc pg_pass postgres
 diset tpcc pg_dbase postgres
 diset tpcc pg_tspace pg_default
-diset tpcc pg_storedprocs false
+diset tpcc pg_raiseerror true
 
-puts "SCHEMA BUILD STARTED"
-buildschema
-puts "SCHEMA BUILD COMPLETED"
+
+
+puts "DROP SCHEMA STARTED"
+deleteschema
+puts "DROP SCHEMA COMPLETED"
